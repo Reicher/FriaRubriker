f03e07f53ad6f2319f2e16d394b24ad194bb5a8f
Robin Reicher <Robin.Reicher@westermo.se>
2018-08-21 16:56:37 +0200

    Split words into new file
    Signed-off-by: Robin Reicher <Robin.Reicher@westermo.se>

---
+++ words.js
@@ -0,0 +1,7 @@
+var words = [
+    ["Avslöjar:", "Skakig mobilkamera:", "Chocken:", "Folkets fiende:", "KAOSET:", "Sveket:", "Mardrömmen:", "Skräcken:", "Flyktingkrisen:", "Maktmissbruket:", "Systemkolapsen:"],
+    ["Kakan Hermansson", "Muslimer", "Islamister", "Miljöpartiet", "Känd feminist", "Hemlig källa", "Conny, 54", "Facket", "Sjuklövern", "Romerna", "Judarna", "Vänsterpartiet", "Stefan Löfven", "AFA", "Asylsökanden", "Muhammed", "SVT"],
+    ["hyllar", "bagatelliserar", "feminiserar", "mörkar", "filmade", "tystar ned", "talar ut om", "uppmuntrar", "glädjs av", "islamiserar", "uppskattar", "stöttade", "ansvarig för"],
+    ["feministiska", "ryska", "hemliga", "odemokratiska", "livsfarliga", "osannolika", "galna", "förödande", "omtalade", "skrämmande", "okulta", "naiva", "arabiska", "kommunistiska"],
+    ["sharialagarna", "osvenska värden", "kommunististsympatisören", "kulturen", "tiggarligan", "Pridefestivalen", "straffskalan", "våldtäkten", "våldtäkten", "våldtäkten", "regeringskrisen"]
+];
